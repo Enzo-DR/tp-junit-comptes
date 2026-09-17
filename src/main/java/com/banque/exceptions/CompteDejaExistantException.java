63c761858cc1f72d126d9afe956b5e0f12c334c3
Enzo <enzo.draux@edu.saintluc-cambrai.com>
2026-09-17 10:21:38 +0200

    feat: création des exceptions métier (MontantInvalide, SoldeInsuffisant, CompteDejaExistant, CompteInconnu)

--- src/main/java/com/banque/exceptions/CompteDejaExistantException.java
+++ src/main/java/com/banque/exceptions/CompteDejaExistantException.java
@@ -1,5 +1,4 @@
-package exceptions;
-
+package com.banque.exceptions;
 public class CompteDejaExistantException extends RuntimeException {
     public CompteDejaExistantException(String message) {
         super(message);
--- src/main/java/com/banque/exceptions/SoldeInsuffisantException.java
+++ src/main/java/com/banque/exceptions/SoldeInsuffisantException.java
@@ -1,7 +1,7 @@
 package com.banque.exceptions;
 
 public class SoldeInsuffisantException extends RuntimeException {
-  public SoldeInsuffisantException(String message) {
-    super(message);
-  }
+    public SoldeInsuffisantException(String message) {
+        super(message);
+    }
 }
